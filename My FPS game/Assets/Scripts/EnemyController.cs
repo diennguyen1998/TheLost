a927408e531e6fa1678efb1b7235ce069065e732
diennguyen1998 <diennguyentdt@gmail.com>
2019-09-15 13:41:24 +0700

    Update 15/9/2019

--- My FPS game/Assets/Scripts/EnemyController.cs
+++ My FPS game/Assets/Scripts/EnemyController.cs
@@ -5,17 +5,55 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    private NavMeshAgent agent;
     public GameObject target;
+    private const float chasingDelay = 2.5f;
+    private float lookRadius = 10f;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        FaceTarget();
+        Invoke("Chase", chasingDelay);
+    }
+
+    void Chase()
     {
-        agent.SetDestination(target.transform.position);
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            //anim.SetBool("isScreaming", false);
+            agent.SetDestination(target.transform.position);
+            agent.speed = 3.5f;
+            if (distance <= agent.stoppingDistance)
+            {
+                anim.SetBool("isAttacking", true);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", false);
+            }
+        }
+        
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
--- My FPS game/Assets/Scripts/GameManager.cs
+++ My FPS game/Assets/Scripts/GameManager.cs
@@ -7,9 +7,21 @@ public class GameManager : MonoBehaviour
 {
     public float restartDelay = 1f;
     public GameObject deadUI;
+    private EnemyController enemyController;
+    private PlayerMove playerMove;
+    private PlayerView playerView;
+
+    void Start()
+    {
+        enemyController = FindObjectOfType<EnemyController>();
+        playerMove = FindObjectOfType<PlayerMove>();
+        playerView = FindObjectOfType<PlayerView>();
+    }
 
     public void EndGame()
     {
+        playerMove.enabled = false;
+        enemyController.enabled = false;
         deadUI.SetActive(true);
         Invoke("Restart", restartDelay);
     }
