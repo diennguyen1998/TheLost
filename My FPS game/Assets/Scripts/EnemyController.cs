54a8ee04c94e9d04e85c7f1142f2edefc2c0c56c
diennguyen1998 <diennguyentdt@gmail.com>
2019-09-15 22:10:29 +0700

    Update 15/9/2019

--- My FPS game/Assets/Scripts/EnemyController.cs
+++ My FPS game/Assets/Scripts/EnemyController.cs
@@ -11,18 +11,30 @@ public class EnemyController : MonoBehaviour
     private float lookRadius = 15f;
     private Animator anim;
     public GameManager gameManager;
+    private float wanderRadius = 7f;
+    private Vector3 wanderPoint;
+    private bool isAware = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
+        wanderPoint = RandomWanderPoint();
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         FaceTarget();
         Invoke("Chase", chasingDelay);
+        /*if (isAware)
+        {
+            
+        }
+        else
+        {
+            Wander();
+        }*/
     }
 
     void Chase()
@@ -30,7 +42,6 @@ public class EnemyController : MonoBehaviour
         float distance = Vector3.Distance(target.transform.position, transform.position);
         if (distance <= lookRadius)
         {
-            //anim.SetBool("isScreaming", false);
             agent.SetDestination(target.transform.position);
             agent.speed = 3.5f;
             if (distance <= agent.stoppingDistance)
@@ -57,5 +68,26 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void Wander()
+    {
+        if(Vector3.Distance(transform.position, wanderPoint) < 0.5f)
+        {
+            wanderPoint = RandomWanderPoint();
+        }
+        else
+        {
+            Debug.Log("Wander");
+            agent.SetDestination(wanderPoint);
+        }
+    }
+
+    private Vector3 RandomWanderPoint()
+    {
+        Vector3 randomPoint = (Random.insideUnitSphere * wanderRadius) + transform.position;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomPoint, out navHit, wanderRadius, -1);
+        return new Vector3(navHit.position.x, transform.position.y, navHit.position.z);
     }
 }
--- My FPS game/Assets/Scripts/GameManager.cs
+++ My FPS game/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public float restartDelay = 1f;
     public GameObject deadUI;
+    public GameObject enemy;
     private EnemyController enemyController;
     private PlayerMove playerMove;
     private PlayerView playerView;
@@ -16,6 +17,7 @@ public class GameManager : MonoBehaviour
         enemyController = FindObjectOfType<EnemyController>();
         playerMove = FindObjectOfType<PlayerMove>();
         playerView = FindObjectOfType<PlayerView>();
+        EnemyDrop();
     }
 
     public void EndGame()
@@ -34,5 +36,12 @@ public class GameManager : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void EnemyDrop()
+    {
+        int xPos = Random.Range(11, 50);
+        int zPos = Random.Range(11, 31);
+        enemy.transform.position = new Vector3(xPos, 0, zPos);
     }
 }
